2b310387934f5250e849abb54d670833e0ef25ad
klepnev-ea <klepnev-ea>
2021-01-23 18:05:46 +0300

    edit main post page

--- src/app/components/post-page/post-page.component.ts
+++ src/app/components/post-page/post-page.component.ts
@@ -1,7 +1,9 @@
 
 
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { IPost } from 'src/app/shared/interfaces';
 import { PostService } from 'src/app/shared/services/post.services';
 
 
@@ -12,13 +14,20 @@ import { PostService } from 'src/app/shared/services/post.services';
 })
 export class PostPageComponent implements OnInit {
 
+  public post!: IPost
+
   constructor(
     private route: ActivatedRoute,
     private postService: PostService
   ) { }
 
   ngOnInit(): void {
-
+    this.route.params
+    .pipe(switchMap((params: Params)=> {
+      return this.postService.getPostById(params['id'])
+    })).subscribe((post: IPost)=> {
+      this.post = post
+    })
   }
 
 }
